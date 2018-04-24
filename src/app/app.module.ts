4d82396ff6faef3ebb907cc55db073b1d7c4b780
fernandobarbosapinto <fernando.mcsa@gmail.com>
2018-04-23 21:14:29 -0300

    Ajustando o padrão currency para pt-BR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID} from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { ROUTES } from './app.routes'; 
@@ -37,7 +37,7 @@ import { CarrinhoComprasService } from './content-restaurantes/carrinho-compras/
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantesService, CarrinhoComprasService],
+  providers: [RestaurantesService, CarrinhoComprasService, {provide: LOCALE_ID, useValue:'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
