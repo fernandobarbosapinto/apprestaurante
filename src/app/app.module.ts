5d29e5ae2426948cfc7365cb33821242cdf8963c
fernandobarbosapinto <fernando.mcsa@gmail.com>
2018-05-03 20:03:38 -0300

    Criando componente ControlValueAccessor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { ReviewsComponent } from './content-restaurantes/reviews/reviews.compone
 import { CarrinhoComprasService } from './content-restaurantes/carrinho-compras/carrinho-compras.service';
 import { OrderComponent } from './order/order.component';
 import { InputComponent } from './shared/input/input.component';
+import { RadioComponent } from './shared/radio/radio.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { InputComponent } from './shared/input/input.component';
     MenuItemComponent,
     ReviewsComponent,
     OrderComponent,
-    InputComponent
+    InputComponent,
+    RadioComponent
   ],
   imports: [
     BrowserModule,
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from '../shared/radio/radio-option.model';
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+  paymentOptions: RadioOption[] = [
+    {label:'Dinheiro', value: 'MON'},
+    {label:'Cartão de Débito', value: 'DEB'},
+    {label:'Cartão de Refeição', value: 'REF'}
+  ];
 
   constructor() { }
 
