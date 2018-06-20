78b3ccaceba54876b1b7eb710c384928b041b338
Fernando <fernando.mcsa@gmail.com>
2018-06-20 09:35:26 -0300

    Criando rotas com Wildcard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { ReviewsComponent } from './content-restaurantes/reviews/reviews.compone
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 import { SharedModule } from './shared/shared.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
@@ -32,7 +33,8 @@ import { SharedModule } from './shared/shared.module';
     CarrinhoComprasComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderSummaryComponent
+    OrderSummaryComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { ContentRestaurantesComponent } from './content-restaurantes/content-res
 import { MenuComponent } from './content-restaurantes/menu/menu.component';
 import { ReviewsComponent } from './content-restaurantes/reviews/reviews.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
@@ -19,5 +20,6 @@ export const ROUTES: Routes = [
     },
     {path: 'about', loadChildren: './about/about.module#AboutModule'},
     {path: 'order-summary', component: OrderSummaryComponent},
-    {path: 'order', loadChildren: './order/order.module#OrderModule'}
+    {path: 'order', loadChildren: './order/order.module#OrderModule'},
+    {path: '**', component: NotFoundComponent}
 ]
